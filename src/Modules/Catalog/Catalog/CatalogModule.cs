ad240d82f14376faa8acc49e1e874826a13e03e9
Ali <pachinco.a.m@gmail.com>
2024-11-21 19:35:29 +0330

    Implementing Infrastructure for Catalog module - Adding DbContext - Seeding initial data - Implementing interceptors - Implementing dispatch operation in infrastructure

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data;
+using Shared.Data.Interceptors;
 
 namespace Catalog;
 
@@ -8,11 +11,34 @@ public static class CatalogModule
 {
     public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
     {
+        // Application use case services
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        // Data - Infrastructure services
+        var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<CatalogDbContext>((sp, options)=>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseNpgsql(connectionString);
+        });
+
+        services.AddScoped<IDataSeeder, CatalogDataSeeder>();
+
         return services;
     }
 
     public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
     {
+        // Use Data - Infrastructure services
+        app.UseMigration<CatalogDbContext>();
+
         return app;
     }
 }
---
+++ src/Modules/Catalog/Catalog/Data/Seed/CatalogDataSeeder.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.Data.Seed;
+
+public class CatalogDataSeeder : IDataSeeder
+{
+    private readonly CatalogDbContext _dbContext;
+
+    public CatalogDataSeeder(CatalogDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task SeedAllAsync()
+    {
+        if (!await _dbContext.Products.AnyAsync())
+        {
+            await _dbContext.Products.AddRangeAsync(InitialData.Products);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
